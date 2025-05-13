8aa2e0a8d7d6e1f7bf421c0f1e15393be6de4fdd
atercates <atercates@ater-ai.home>
2025-05-13 23:33:07 +0200

    Itoa done

--- itoa/ft_itoa.c
+++ itoa/ft_itoa.c
@@ -6,29 +6,39 @@
 /*   By: javifer2 <javifer2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 20:09:53 by javifer2          #+#    #+#             */
-/*   Updated: 2025/05/13 20:58:42 by javifer2         ###   ########.fr       */
+/*   Updated: 2025/05/13 23:25:05 by javifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include <libft.h>*/
-#include <stdio.h>
+#include <libft.h>
 #include <stdlib.h>
 
-int	get_length(int n)
+static int	get_length(long n)
 {
 	int	i;
+	int	negative;
 
 	i = 0;
+	negative = 0;
+	if (n < 0)
+	{
+		n = -n;
+		negative = 1;
+	}
 	while (n >= 10)
 	{
 		n = n / 10;
 		i++;
 	}
+	if (negative)
+		return (i + 3);
 	return (i + 2);
 }
 
-void	fill_str(char *result, int n, int p)
+static void	fill_str(char *result, long n, int p)
 {
+	if (n < 0)
+		n = -n;
 	if (n >= 10)
 	{
 		result[p] = (char)(n % 10 + '0');
@@ -42,31 +52,38 @@ char	*ft_itoa(int n)
 {
 	char	*result;
 	int		size;
-	
-	size = get_length(n);
+	long	nb;
+
+	nb = n;
+	size = get_length(nb);
 	result = malloc(size * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 	result[size - 1] = '\0';
-	fill_str(result, n, size - 2);
+	if (n < 0)
+		result[0] = '-';
+	fill_str(result, nb, size - 2);
 	return (result);
 }
 
+/*
+#include <stdio.h>
 int	main(int argc, char **argv)
 {
 	int	length;
+	long	num;
 	char  *result;
 
 	if (argc != 2)
+	{
 		printf("Parametro invalido. Uso %s 'NUM'", argv[0]);
 		return (1);
-	length = get_length(argv[1]);
-	printf("Reservando %d bytes de memoria para %d\n", length, argv[1]);
-	result = ft_itoa(argv[1]);
+	}
+	num = ft_atoi(argv[1]); 
+	length = get_length(num);
+	printf("Reservando %d bytes de memoria para %ld\n", length, num);
+	result = ft_itoa(num);
 	printf("La string resultante es %s\n", result);
 	free(result);
 	return (0);
-}
-
-
-
+}*/
