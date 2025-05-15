199432ad4b276dd3fb92bd1b76cf98b0471da9b5
atercates <atercates@ater-ai.home>
2025-05-15 14:20:15 +0200

    Pendiente de revisar errores

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: javifer2 <javifer2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:52:37 by javifer2          #+#    #+#             */
-/*   Updated: 2025/05/11 17:09:06 by javifer2         ###   ########.fr       */
+/*   Updated: 2025/05/15 14:11:39 by javifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	if (!needle[i])
-		return (haystack);
+		return ((char *)haystack);
 	while (i < len)
 	{
 		back = i;
@@ -31,7 +31,7 @@ char	*strnstr(const char *haystack, const char *needle, size_t len)
 			j++;
 		}
 		if (needle[j] == '\0')
-			return (haystack + back);
+			return ((char *)haystack + back);
 		i = back + 1;
 	}
 	return (NULL);
