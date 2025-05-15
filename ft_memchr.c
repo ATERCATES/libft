c6b1592d9d005bef81b4afadde35e837637d9de5
atercates <atercates@ater-ai.home>
2025-05-15 21:55:27 +0200

    Strncmp y memchr fixed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: javifer2 <javifer2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:11:05 by javifer2          #+#    #+#             */
-/*   Updated: 2025/05/15 19:34:36 by javifer2         ###   ########.fr       */
+/*   Updated: 2025/05/15 21:54:44 by javifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	p = (unsigned char *)s;
 	i = 0;
-	while (i < n && p[i] != (unsigned char)c)
+	while (i + 1 < n && p[i] != (unsigned char)c)
 		i++;
 	if (p[i] == (unsigned char)c)
 		return ((void *)(p + i));
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,11 +6,11 @@
 /*   By: javifer2 <javifer2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 15:34:38 by javifer2          #+#    #+#             */
-/*   Updated: 2025/05/15 19:34:24 by javifer2         ###   ########.fr       */
+/*   Updated: 2025/05/15 21:48:38 by javifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft.h" 
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -19,5 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && s1[i] && (unsigned char)s1[i] == (unsigned char)s2[i])
 		i++;
+	if (i == n)
+		return ((unsigned char)s1[i - 1] - (unsigned char)s2[i - 1]);
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
