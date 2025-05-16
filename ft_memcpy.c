5c19b452172a5cc97ad3047add572586a5befab0
Javier Fernandez Marino <javifer2@cbr7s5.42barcelona.com>
2025-05-16 18:34:58 +0200

    test memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: javifer2 <javifer2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 19:29:23 by javifer2          #+#    #+#             */
-/*   Updated: 2025/05/10 19:38:16 by javifer2         ###   ########.fr       */
+/*   Updated: 2025/05/16 18:34:52 by javifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*ptrsrc;
 	size_t			i;
 
-	if (!dst || !src)
+	if (!dst && !src)
 		return (NULL);
 	i = 0;
 	ptrdst = (unsigned char *)dst;
