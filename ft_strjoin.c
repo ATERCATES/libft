c0b08477490d15ea4d1be1b745d6a78de4bdf9ce
ATERCATES <atercates@gmail.com>
2025-10-10 22:34:33 +0200

    Improved and protected ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: javifer2 <javifer2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 11:17:09 by javifer2          #+#    #+#             */
-/*   Updated: 2025/05/22 19:09:49 by javifer2         ###   ########.fr       */
+/*   Updated: 2025/10/10 22:34:12 by javifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	j;
 	char	*ptr;
 
+	if (!s1 || !s2)
+		return (NULL);
 	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (s1[i])
-	{
-		ptr[j] = s1[i];
-		i++;
-		j++;
-	}
+		ptr[j++] = s1[i++];
 	i = 0;
 	while (s2[i])
-	{
-		ptr[j] = s2[i];
-		i++;
-		j++;
-	}
+		ptr[j++] = s2[i++];
 	ptr[j] = '\0';
 	return (ptr);
 }
